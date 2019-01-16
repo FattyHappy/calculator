94e0f329de1543da677b28c9923bf4ef47a83d2e
zhber <624179076@qq.com>
2019-01-16 11:57:40 +0800

    fix .gitignore

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#include"lib/fastIO.hpp"
+using namespace fastIO;
 typedef long long ll;
 const static double eps=1e-8;
 const static int operatorNum=10;
@@ -22,14 +24,6 @@ public:
     treeNode(){lSon=rSon=NULL;}
     ~treeNode(){}
     char nodeData;
-	char getData()
-	{
-
-	}
-    void setData()
-    {
-
-    }
     expr gen()
     {
         if (nodeData>='0'&&nodeData<='9')return expr(std::string(&nodeData,1),nodeData-'0');
@@ -54,33 +48,36 @@ public:
 class decisionTree{
 public:
     treeNode* root;
-    decisionTree()//构造函数：随机生成一个判定树
+    inline void init()//构造函数：随机生成一个判定树
 	{
         register int point[operatorNum];
         for (register int i=0;i<operatorNum;++i)point[i]=i;
         for (register int i=0;i<10*operatorNum;++i)std::swap(point[rnd(10)],point[rnd(10)]);
-		root=new treeNode[operatorNum];
+		treeNode** points=new treeNode*[operatorNum];
+		for (register int i=0;i<operatorNum;i++)points[i]=new treeNode;
         std::vector<int>nonEmptyPoint;
         nonEmptyPoint.push_back(point[0]);nonEmptyPoint.push_back(point[0]);
         for (register int i=1;i<operatorNum;i++)
 		{
             register int tmpPos=rnd(nonEmptyPoint.size());
-            register treeNode* tmpPoint=&root[nonEmptyPoint[tmpPos]];
-            if(tmpPoint->lSon==NULL&&tmpPoint->rSon==NULL) (rnd(2)?tmpPoint->lSon:tmpPoint->rSon)=&root[point[i]];
-            else (tmpPoint->lSon==NULL?tmpPoint->lSon:tmpPoint->rSon)=&root[point[i]];
+            register treeNode* tmpPoint=points[nonEmptyPoint[tmpPos]];
+            if(tmpPoint->lSon==NULL&&tmpPoint->rSon==NULL) (rnd(2)?tmpPoint->lSon:tmpPoint->rSon)=points[point[i]];
+            else (tmpPoint->lSon==NULL?tmpPoint->lSon:tmpPoint->rSon)=points[point[i]];
 			nonEmptyPoint.erase(nonEmptyPoint.begin()+tmpPos);
             nonEmptyPoint.push_back(point[i]);nonEmptyPoint.push_back(point[i]);
 		}
-		root=&root[point[0]];
+		root=points[point[0]];
+        delete[] points;
 		//=================手动栈模拟dfs=============================
 		std::stack<treeNode*>nodeStack;
 		std::stack<int>stat;
         nodeStack.push(root);stat.push(1);
         while (!nodeStack.empty())
 		{
+            //printf("%d %d %d\n",nodeStack.top(),nodeStack.top()->lSon,nodeStack.top()->rSon);
             if (stat.top()==1)
 			{
-
+				//puts("init 1");
 				stat.pop();stat.push(2);
 				if (nodeStack.top()->lSon!=NULL)
 				{
@@ -95,6 +92,7 @@ public:
 			}
             if (stat.top()==2)
 			{
+                //puts("init 2");
                 stat.pop();stat.push(3);
                 if (nodeStack.top()->rSon!=NULL)
 				{
@@ -109,6 +107,7 @@ public:
 			}
             if (stat.top()==3)
 			{
+				//puts("init 3");
                 nodeStack.top()->nodeData=operators[rnd(4)];
 				nodeStack.pop();
 				stat.pop();
@@ -125,33 +124,36 @@ public:
 		{
             if (stat.top()==1)
 			{
-
+                //puts("solve 1");
 				stat.pop();stat.push(2);
 				if (nodeStack.top()->lSon!=NULL)
 				{
-					stat.push(1);
 					nodeStack.push(nodeStack.top()->lSon);
+					stat.push(1);
 					continue;
 				}
 			}
             if (stat.top()==2)
 			{
+                //puts("solve 2");
                 stat.pop();stat.push(3);
                 if (nodeStack.top()->rSon!=NULL)
 				{
-					stat.push(1);
 					nodeStack.push(nodeStack.top()->rSon);
+					stat.push(1);
 					continue;
 				}
 			}
             if (stat.top()==3)
 			{
-				delete nodeStack.top();
+                treeNode* tp=nodeStack.top();
 				nodeStack.pop();
 				stat.pop();
+				delete tp;
 			}
 		}
     }
+    decisionTree(){init();}
 	~decisionTree(){erase();}
     expr gen()//根据判定树生成对应计算表达式
     {
@@ -159,24 +161,38 @@ public:
         while (tmpExpr==NUL)//出现除0的情况就是不合法 重造数据
 		{
 			erase();
-			decisionTree();
+			init();
 			tmpExpr=root->gen();
 		}
     	return root->gen();
     }
 };
-
-inline void init()
+int num;
+inline void init(int argc,char** argv)
 {
+	assert(argc==3);
+    assert(refreshOut((const char*)argv[1]));
+    int len = int(strlen(argv[2]));
+    num = 0;
+	for (int i = 0; i < len; i++)
+	{
+		assert(argv[2][i]>='0'&&argv[2][i]<='9');
+		num = num * 10 + argv[2][i] - '0';
+	}
+    assert(num>=1&&num<=1000);
 	priority['+']=priority['-']=1;
 	priority['*']=priority['/']=2;
     srand(time(0)^19260817);
 }
-int main()
+int main(int argc,char** argv)
 {
-    init();
-    decisionTree now;
-    expr g=now.gen();
-    std::cout<<g.exprString<<std::endl;
-    printf("%f\n",g.ans);
+    init(argc,argv);
+    for (int T=1;T<=num;T++)
+	{
+        decisionTree now;
+        expr ans=now.gen();
+		for (int i=0;i<ans.exprString.length();i++)print(ans.exprString[i]);
+        print('\n');
+        //for (int i=0;i<ans.exprString.length();i++)print(ans.exprString[i]);
+	}
 }
